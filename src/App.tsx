fe91185e98550cfd37aed471a221a7294ef0f105
Олексанр Чумак <cauua@air-oleksanr.lan>
2023-11-12 20:49:52 +0100

    Added tailwind

--- src/App.tsx
+++ src/App.tsx
@@ -2,9 +2,9 @@ import React from 'react';
 
 function App(): React.JSX.Element {
   return (
-    <>
+    <div className="text-3xl font-bold h-3 text-red-500">
       main page
-    </>
+    </div>
   );
 }
 
